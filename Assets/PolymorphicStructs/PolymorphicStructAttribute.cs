55576748f6f348d7c00d3e489a437d03e04e7aa9
Philippe St-Amand <philippe.stamand1@gmail.com>
2022-07-04 10:30:37 -0400

    Added "NUnit3TestAdapter" package for tests, removed useless "implementedInterfaces" param from from PolymorphicStruct attr

--- Assets/PolymorphicStructs/PolymorphicStructAttribute.cs
+++ Assets/PolymorphicStructs/PolymorphicStructAttribute.cs
@@ -6,10 +6,4 @@ using UnityEngine;
 [AttributeUsage(AttributeTargets.Interface)]
 public class PolymorphicStruct : System.Attribute
 {
-    public string ImplementedInterfaces; 
-
-    public PolymorphicStruct(string implementedInterfaces = "")
-    {
-        ImplementedInterfaces = implementedInterfaces;
-    }
 }
